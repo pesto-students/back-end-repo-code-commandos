0e4c76939d853fad4aaa5c3a487ce1207e5def61
nsummit <nsummit10@gmail.com>
2024-01-06 07:33:07 +0530

    Setting up index

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const server = http.createServer(app);
 const socketID = require("socket.io");
 
 const io = socketID(server, {
-  cors: {
-    origin: "http://localhost:5173",
-    credentials: true,
-  },
+	cors: {
+		origin: process.env.ORIGIN,
+		credentials: true,
+	},
 });
 
 const cors = require("cors");
@@ -30,16 +30,18 @@ const Chat = require("./models/chat.model");
 require("dotenv").config();
 
 app.use(cookieParser());
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-}));
+app.use(
+	cors({
+		origin: process.env.ORIGIN,
+		credentials: true,
+	})
+);
 app.use(express.json());
 
 dbConnect();
 
 app.get("/*", (req, res) => {
-  res.send("Error 404. Page Not Found");
+	res.send("Error 404. Page Not Found");
 });
 
 app.use("/user", usersRouter);
@@ -48,31 +50,33 @@ app.use("/message", messagesRouter);
 app.use("/friends", friendsRouter);
 
 io.on("connection", (socket) => {
-  console.log(socket.id);
-  socket.on("get-id", (Id) => {
-    console.log("IDs " + Id.chatId);
-  });
+	console.log(socket.id);
+	socket.on("get-id", (Id) => {
+		console.log("IDs " + Id.chatId);
+	});
 
-  socket.on("message", (newMessage) => {
-    console.log("Receiver message:" + newMessage.texts);
-    createMessage(newMessage);
-  });
+	socket.on("message", (newMessage) => {
+		console.log("Receiver message:" + newMessage.texts);
+		createMessage(newMessage);
+	});
 });
 
 const createMessage = async (message) => {
-  const { chatId, senderid, texts } = message;
-  console.log(chatId, senderid, texts);
+	const { chatId, senderid, texts } = message;
+	console.log(chatId, senderid, texts);
 
-  await Message.create({
-    chatId: chatId,
-    senderId: senderid,
-    text: texts,
-  })
-    .then((response) => {
-      console.log(response);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+	await Message.create({
+		chatId: chatId,
+		senderId: senderid,
+		text: texts,
+	})
+		.then((response) => {
+			console.log(response);
+		})
+		.catch((error) => {
+			console.log(error);
+		});
 };
-server.listen(3000, () => console.log("Listening on port 3000..."));
+server.listen(process.env.PORT, () =>
+	console.log(`Listening on port ${process.env.PORT}...`)
+);
